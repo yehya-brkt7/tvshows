e33eaab990fde4a4cf896df11237b0629f57cf59
Yehya brkt <yaya.bt747@gmail.com>
2023-04-01 18:11:03 +0300

    update

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,5 @@
 const User = require("../models/userModel");
-const List = require("../models/userModel");
+// const List = require("../models/userModel");
 const mongoose = require("mongoose");
 
 //post one show
